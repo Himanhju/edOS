f1ec11332f4e4facf5222e81d06555a0e6727812
Himanhju <152820359+Himanhju@users.noreply.github.com>
2025-05-05 15:28:36 -0400

    Update edOS.c

--- edOS.c
+++ edOS.c
@@ -1,1 +1,2 @@
 // remember to update the mkbin function in ~/.bashrc
+// run with mkbin -a boot.asm; mkbin -ac A400 edOS.asm ... all C files; mkimg boot.asm.bin edOS.bin img.img; runbin img.img edOS.fs;  # only for developer, final version will have a build file
